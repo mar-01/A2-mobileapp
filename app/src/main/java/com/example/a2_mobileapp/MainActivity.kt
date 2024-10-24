17556e65d44907295a284be1e37f8dc01903e5a7
Marcel Madlener <marcelmadlener@gmx.de>
2024-10-24 23:19:17 +0200

    Verbindung zu Firebase hergestellt

--- app/src/main/java/com/example/a2_mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/a2_mobileapp/MainActivity.kt
@@ -24,6 +24,7 @@ class MainActivity : ComponentActivity() {
                     Toast.makeText(this, "Daten erfolgreich gespeichert", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this, "Fehler: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+
                 }
             }
         }
