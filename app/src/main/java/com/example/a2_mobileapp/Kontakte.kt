f0ad38a6eb4413d3b8bfe61359b4954fbf60693e
Marcel Madlener <marcelmadlener@gmx.de>
2024-10-25 04:04:39 +0200

    Kontakte in die DB hochladen

---
+++ app/src/main/java/com/example/a2_mobileapp/Kontakte.kt
@@ -0,0 +1,7 @@
+package com.example.a2_mobileapp
+
+data class Kontakte(
+    val id: String? = null,
+    val name: String? = null,
+    val nummer: String? = null
+)
--- app/src/main/java/com/example/a2_mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/a2_mobileapp/MainActivity.kt
@@ -3,11 +3,6 @@ package com.example.a2_mobileapp
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.a2_mobileapp.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -23,15 +18,28 @@ class MainActivity : ComponentActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseRef = FirebaseDatabase.getInstance("https://a2-mobileapp-default-rtdb.europe-west1.firebasedatabase.app").getReference("test")
-        binding.button.setOnClickListener {
+        firebaseRef = FirebaseDatabase.getInstance("https://a2-mobileapp-default-rtdb.europe-west1.firebasedatabase.app").getReference("Kontakte")
+        binding.btnSenddata.setOnClickListener {
             firebaseRef.setValue("test").addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "Daten erfolgreich gespeichert", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, "Fehler: ${task.exception?.message}", Toast.LENGTH_LONG).show()
-                }
+                senddata()
             }
         }
+    }
+
+    private fun senddata(){
+        val name = binding.textName.text.toString()
+        val nummer = binding.textNummer.text.toString()
+
+        if (name.isEmpty()) binding.textName.error = "Name einfügen"
+        if (nummer.isEmpty()) binding.textNummer.error = "Nummer einfügen"
+
+        val kontaktId = firebaseRef.push().key!!
+        val kontakt = Kontakte(kontaktId, name, nummer)
+
+        firebaseRef.child(kontaktId).setValue(kontakt)
+            .addOnCompleteListener {
+                Toast.makeText(this, "Daten hochgeladen", Toast.LENGTH_SHORT).show()
+            }
+
     }
 }
